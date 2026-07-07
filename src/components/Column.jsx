e737d31bfdf99996579baa258029daeebe25a99f
Hritang Sugandh <hritang24264@iiitd.ac.in>
2026-07-07 13:05:16 +0530

    Render task cards from React state

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import TaskCard from "./TaskCard";
 
 function Column(props) {
+
+  const [tasks, setTasks] = useState([
+    "Learn React"
+  ]);
 
   return (
     <div className="column">
 
       <h2>{props.title}</h2>
 
-      <TaskCard title="Learn React" />
+      {tasks.map((task) => (
+        <TaskCard
+          key={task}
+          title={task}
+        />
+      ))}
 
     </div>
   );
-
 }
 
 export default Column;
